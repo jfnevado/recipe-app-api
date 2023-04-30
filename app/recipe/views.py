0a56e694c646dd15ecf946979b1d77b5a0651ec0
rCer <progravado@gmail.com>
2023-04-30 06:40:49 +0200

    Fixed Lint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -47,6 +47,7 @@ class TagViewSet(mixins.DestroyModelMixin,
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
